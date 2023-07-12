7459994ec1011557aeef93a6df6509479ee94c14
pavincius <30844451+PAVincius@users.noreply.github.com>
2023-07-12 19:56:30 -0300

    Update astro.config.mjs

--- Tema1/astro.config.mjs
+++ Tema1/astro.config.mjs
@@ -7,5 +7,5 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: 'https://pavincius.github.io',
-  base: '/Tema1'
+  base: '/tema1'
 });
